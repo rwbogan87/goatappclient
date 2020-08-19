bd521a2499d3bd3fb76f5d8977effe1fee61368f
ryan <user@superbook.com>
2020-08-18 22:27:30 -0400

    router built, roster entered; added reactstrap and proptypes packages

--- src/Components/Roster/roster.js
+++ src/Components/Roster/roster.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Table } from 'reactstrap'
+import PropTypes from 'prop-types';
 import './roster.css'
 
 // Running Roster - *Make changes here*
@@ -7,60 +9,76 @@ const Roster = (props) => {
     return (
         <div className="Roster-div">
         <h2 className="Roster-header">League Roster</h2>
-        <table className="Roster-table">
-        <tbody>
+        <h4>Champs Listed First</h4>
+        <Table hover>
+        <thead>
             <tr>
+                <th>Championships</th>
                 <th>Team</th>
                 <th>Owner</th>
             </tr>
-            <tr className="one">
-                <td>Minshews Mustache</td>
-                <td>Nathan Bogan</td>
-            </tr>
+        </thead>
+        <tbody>
             <tr>
-                <td>Quenton Quarantino</td>
-                <td>Ryan Bogan</td>
-            </tr>
-            <tr className="one">
-                <td>Cyka Blyat</td>
-                <td>Alec Christensen</td>
-            </tr>
-            <tr>
+            <th scope="row">3</th>
                 <td>Stafford Rams Lutz</td>
                 <td>Caleb Christensen</td>
             </tr>
-            <tr className="one">
-                <td>Trust Me Im a Doctor</td>
-                <td>Richard Christensen</td>
-            </tr>
             <tr>
-                <td>Krafts Korean Koochie</td>
-                <td>Alex Decaudin</td>
-            </tr>
-            <tr className="one">
+            <th scope="row">2</th>
                 <td>TDs Make My Vick Hard</td>
                 <td>Branden Irvin</td>
             </tr>
             <tr>
-                <td>Chubby Johnson</td>
-                <td>Jay Jennings</td>
-            </tr>
-            <tr className="one">
-                <td>Browns Cumback Story</td>
-                <td>Josh Petriko</td>
+                <th scope="row">1</th>
+                <td>Quenton Quarantino</td>
+                <td>Ryan Bogan</td>
             </tr>
             <tr>
+            <th scope="row">1</th>
                 <td>Take Mahomes Country Roads</td>
                 <td>Jon Steele</td>
             </tr>
-        </tbody>
-        </table>
-        <br />
-        <table className="Roster-table">
-        <tbody>
             <tr>
-                <th>Retiree</th>
+                <th scope="row"></th>
+                <td>Minshews Mustache</td>
+                <td>Nathan Bogan</td>
             </tr>
+            <tr>
+            <th scope="row"></th>
+                <td>Cyka Blyat</td>
+                <td>Alec Christensen</td>
+            </tr>
+            <tr>
+            <th scope="row"></th>
+                <td>Trust Me Im a Doctor</td>
+                <td>Richard Christensen</td>
+            </tr>
+            <tr>
+            <th scope="row"></th>
+                <td>Krafts Korean Koochie</td>
+                <td>Alex Decaudin</td>
+            </tr>
+            <tr>
+            <th scope="row"></th>
+                <td>Chubby Johnson</td>
+                <td>Jay Jennings</td>
+            </tr>
+            <tr>
+            <th scope="row"></th>
+                <td>Browns Cumback Story</td>
+                <td>Josh Petriko</td>
+            </tr>
+        </tbody>
+        </Table>
+        <br />
+        <Table className="Roster-table">
+        <thead>
+            <tr>
+                <th>Retirees</th>
+            </tr>
+        </thead>
+        <tbody>
             <tr className="one">
                 <td>Rob Clark</td>
             </tr>
@@ -74,12 +92,29 @@ const Roster = (props) => {
                 <td>Kelly Williams</td>
             </tr>
         </tbody>
-        </table>
+        </Table>
 
         <p className="Roster-text"></p>
 
         </div>
     )   
 }
- 
+
+Table.propTypes = {
+    // Pass in a Component to override default element
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    size: PropTypes.string,
+    bordered: PropTypes.bool,
+    borderless: PropTypes.bool,
+    striped: PropTypes.bool,
+    dark: PropTypes.bool,
+    hover: PropTypes.bool,
+    responsive: PropTypes.bool,
+    // Custom ref handler that will be assigned to the "ref" of the inner <table> element
+    innerRef: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.string,
+      PropTypes.object
+    ])
+    };
 export default Roster
